8179004a2e3bc098eaa1b06a4908b640deda2961
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-09-06 10:39:00 +0300

    search

--- contact_book.py
+++ contact_book.py
@@ -48,3 +48,21 @@ def view():
         click.echo('No contacts found.')
 
     conn.close()
+
+@cli.command()
+@click.argument('name')
+def search(name):
+    """Search for a contact by name."""
+    conn = sqlite3.connect("contacts.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT contacts.id, contacts.name, contacts.phone, groups.name FROM contacts JOIN groups ON contacts.group_id = groups.id WHERE contacts.name LIKE ?", (f"%{name}%",))
+    contacts = cursor.fetchall()
+
+    if contacts:
+        for contact in contacts:
+            click.echo(f'ID: {contact[0]}, Name: {contact[1]}, Phone: {contact[2]}, Group: {contact[3]}')
+    else:
+        click.echo('No matching contacts found.')
+
+    conn.close()    
